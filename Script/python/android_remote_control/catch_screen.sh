1399deb879eb4a4ed004016869dc709412ca4398
Sven <svenzhang@live.com>
2014-09-25 19:12:49 +0800

    Add Python Tool : AndroidRemoteController
        I want to implement a tool to control android device
        through USB&Adb command
        Based on wxPython library

---
+++ Script/python/android_remote_control/catch_screen.sh
@@ -0,0 +1,2 @@
+sudo adb shell /system/bin/screencap -p /sdcard/screenshot.png
+sudo adb pull /sdcard/screenshot.png screenshot.png
---
+++ Script/python/android_remote_control/mainGui.py
@@ -0,0 +1,166 @@
+import wx
+import time
+import subprocess
+
+app = wx.App(False)
+#frame = wx.Frame(None, wx.ID_ANY, "RemoteController")
+
+btnGapV = 5
+btnGapH = 5
+
+#TODO
+class AdbConnection:
+    def __init__(self):
+        print "hello"
+
+class RemoteDevices:
+    def __init__(self):
+        self.shellFile = "./catch_screen.sh"
+    '''
+    def __init__(self,dName, aCon):
+        self.deviceName = dName 
+        self.adbConnection = aCon
+    '''
+    def UpdateScreen(self):
+        print("Remote Devices Update Screen Begin-->")
+        s = time.clock()
+        subprocess.call([self.shellFile], shell=True)
+        print("Remote Devices Update Screen End <-- %.6f"%(time.clock()-s))
+
+    def Click(self, point):
+        print("Click [%d,%d]"%(point.x,point.y))
+        command="sudo adb shell input touchscreen tap %d %d"%(point.x,point.y)
+        subprocess.call([command], shell=True)
+
+    def ClickHome(self):
+        subprocess.call(["sudo adb shell input keyevent 3"], shell=True)
+
+    def ClickBack(self):
+        subprocess.call(["sudo adb shell input keyevent 3"], shell=True)
+
+    def ClickPower(self):
+        subprocess.call(["sudo adb shell input keyevent 26"], shell=True)
+
+class RemoteView(wx.Window):
+	def __init__(self, parent, id, pos, rDevice):
+		wx.Window.__init__(self,parent,id,pos,size=(1024,600))
+		#self.SetBackgroundColour()
+                self.PngFile = "screenshot.png"
+                self.remoteDevice=rDevice 
+                
+                #Update Timer
+                self.timerUpdate = wx.Timer(self) 
+                self.timerUpdate.Start(5000, False)
+
+		#install event handler
+		wx.EVT_PAINT(self, self.OnPaint)
+		wx.EVT_LEFT_DOWN(self, self.OnMouse)
+		wx.EVT_LEFT_UP(self, self.OnMouse)
+		#wx.EVT_MOTION(self, self.OnMouse)
+		self.Bind(wx.EVT_TIMER, self.OnTimeOut, self.timerUpdate)
+
+	def OnPaint(self, event):
+	    start=time.clock()
+	    self.pngimg = wx.Image( self.PngFile, wx.BITMAP_TYPE_PNG )
+	    self.bmp = wx.BitmapFromImage(self.pngimg)
+            dc = wx.PaintDC(self)
+            dc.DrawBitmap(self.bmp, 0, 0)
+            print("OnPaint Cost %.5f"%(time.clock()-start))
+
+        def OnMouse(self, event):
+            '''
+            if( event.LeftDown() ):
+                print "Left Mouse Down at (%d,%d)"%(event.GetX(),event.GetY())
+            '''
+            if( event.LeftUp() ):
+                #print "Left Mouse Up at (%d,%d)"%(event.GetX(),event.GetY()) 
+                self.remoteDevice.Click(wx.Point(event.GetX(),event.GetY()))
+            #if( event.Moving() ):
+                #print "Mouse moving"
+                #do-nothing
+
+        def OnTimeOut(self,event):
+            print "onTimeOut..."
+            print time.ctime()
+            #1. Get Screen Short 
+            self.remoteDevice.UpdateScreen()
+            self.Refresh()
+
+class MainWindow(wx.Frame):
+	def __init__(self, parent, title):
+		wx.Frame.__init__(self, parent,title=title, size=(300+1024,700) )
+		#1. Layout control
+		preCtrlHeight = 0
+		#1.1 First Column
+		self.deviceList=["0123456789ABCDEF",\
+                   "xxxxxxxxxxxxxxx",\
+                   "0123456789ABCDEF",\
+                   "xxxxxxxxxxxxxxx",\
+                   "0123456789ABCDEF",\
+                   "xxxxxxxxxxxxxxx"\
+                  ]
+
+		self.deviceRadioList = wx.RadioBox(\
+                                     self,\
+                                     id=-1,\
+                                     pos=(btnGapH,btnGapH+preCtrlHeight),\
+                                     choices=self.deviceList,
+                                     majorDimension=1,\
+                                     #size=(300,300),\
+                                     #style =wx.RadioBox.RA_VERTICAL,\
+                                    )
+		self.deviceRadioList.SetSelection( 0 )
+		preCtrlHeight += btnGapV + self.deviceRadioList.GetVirtualSize().GetHeight()
+
+		self.srchDeviceBtn = wx.Button(self, label="Search Devices", pos=(btnGapH,btnGapV+preCtrlHeight) )
+		preCtrlHeight += btnGapV + self.srchDeviceBtn.GetVirtualSize().GetHeight()
+
+		self.resetBtn = wx.Button(self, label="Reset", pos=(btnGapH,preCtrlHeight + btnGapV ))
+		preCtrlHeight += btnGapV + self.resetBtn.GetVirtualSize().GetHeight()
+
+		self.startRemoteContrl = wx.Button(self, id=100, label="Start Remote Contrllor", pos=(btnGapH, preCtrlHeight + btnGapV) )
+		preCtrlHeight += btnGapV + self.resetBtn.GetVirtualSize().GetHeight()
+
+		self.HomeButton = wx.Button(self, label="Home", pos = (btnGapH,preCtrlHeight+btnGapV))
+		preCtrlHeight += btnGapV + self.HomeButton.GetVirtualSize().GetHeight()
+
+		self.BackButton = wx.Button(self, label="Back", pos =(btnGapH,preCtrlHeight+btnGapV))
+		preCtrlHeight += btnGapV + self.BackButton.GetVirtualSize().GetHeight()
+
+		self.PowerButton= wx.Button(self, label="Power", pos=(btnGapH, preCtrlHeight + btnGapV ))
+
+		# Install Event handler
+		self.resetBtn.Bind(wx.EVT_BUTTON, self.onSearchDevices, self.resetBtn)
+		self.srchDeviceBtn.Bind(wx.EVT_BUTTON, self.onSearchDevices, self.srchDeviceBtn)
+		self.HomeButton.Bind(wx.EVT_BUTTON, self.onHomeButtonClick, self.HomeButton)
+		self.BackButton.Bind(wx.EVT_BUTTON, self.onBackButtonClick, self.BackButton)
+		self.PowerButton.Bind(wx.EVT_BUTTON, self.onPowerButtonClick, self.PowerButton)
+
+		#2.2 Graphic View for remote picture
+		#screenShortSize=wx.Size(1024,600)
+	        rDevice = RemoteDevices()	
+		screenShortPos = wx.Point(200,0)
+		self.remoteView = RemoteView(self,101,pos=screenShortPos,rDevice = rDevice)
+		self.remoteDevice = rDevice
+
+
+	def onSearchDevices(self, event):
+		if(self.resetBtn == event):
+			print("OnResetButton")
+		elif(self.srchDeviceBtn == event):
+			print("onSearchDevices")
+		else:
+			print("Nothing")
+	def onHomeButtonClick(self, event):
+	    self.remoteDevice.ClickHome()
+	def onBackButtonClick(self, event):
+	    self.remoteDevice.ClickBack()
+        def onPowerButtonClick(self, event):
+            self.remoteDevice.ClickBack()
+	    
+
+frame = MainWindow(None,"AndroidRemoteController")
+
+frame.Show(True)
+
+app.MainLoop()
